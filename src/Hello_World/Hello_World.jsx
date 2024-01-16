4d460174cb51a5d284efdc40556f94e0dcdb5871
JohnB08 <142892183+JohnB08@users.noreply.github.com>
2024-01-16 21:32:35 +0100

    justerte readme

---
+++ src/Hello_World/Hello_World.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Hello_World.css";
+
+export function HelloWorld() {
+  return <p>Hello World!</p>;
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,5 @@
-
 //!! DET ER VIKTIG DU OGSÅ HER EXPORTERER ALLE KOMPONENTENE SOM VIST UNDER.
-
-
 
 export * from "./Card";
 export * from "./Button";
+export * from "./Hello_World";
