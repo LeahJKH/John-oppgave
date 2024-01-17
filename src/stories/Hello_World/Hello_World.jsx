00c6735f9b86d6d940437ddba7df2e8b6d185594
JohnB08 <142892183+JohnB08@users.noreply.github.com>
2024-01-17 14:39:22 +0100

    mer beskrivende README, begynner med kommentering.

--- src/stories/Hello_World/Hello_World.jsx
+++ src/stories/Hello_World/Hello_World.jsx
@@ -3,7 +3,7 @@ import Style from "./Hello_World.module.css";
 import PropTypes from "prop-types";
 
 export const HelloWorld = ({ primary }) => {
-  const mode = primary ? "Hello World!" : "Goodbye World!";
+  const mode = primary ? "Hello World!" : "Goodnight World!";
   /* Vi bruker klassenavn for å passe på at klassenavnene er unike */
   return <p className={Style.helloWorld}>{mode}</p>;
 };
